95e9694f1af76b85ece19e659935b428cf04aa6b
Amina Hallam <amina.hallam@gmail.com>
2022-01-12 20:34:19 +0100

    Create-for-loop-and-new-function

--- results.php
+++ results.php
@@ -7,6 +7,7 @@ abstract class Animal {
 
     abstract function makeSound();
 
+
 }
 
 class Apa extends Animal {
@@ -36,6 +37,21 @@ class Giraff extends Animal {
 
 }
 
+class Coconut extends Animal {
+    
+    
+    function __construct($name, $picture) {
+        parent::__construct($name, $picture); 
+    }
+    
+    
+    function makeSound() {
+        echo "<script>alert($this -> name) . (' Sounds') . (' PLOOOP')<script>"; 
+        
+    }
+    
+}
+
 class Tiger extends Animal {
 
 
@@ -51,22 +67,55 @@ class Tiger extends Animal {
 
 }
 
-class Coconut extends Animal {
 
+if(isset($_SERVER["REQUEST_METHOD"]) && ($_SERVER["REQUEST_METHOD"] == "POST")) {
 
-    function __construct($name, $picture) {
-        parent::__construct($name, $picture); 
+    if(empty($_POST["Apa"]) && empty($_POST["Giraff"]) && empty($_POST["Coconut"]) && empty($_POST["Tiger"])) {
+
+        echo "Please choose a number"; 
+
     }
-
     
-    function makeSound() {
-        echo "<script>alert($this -> name) . (' Sounds') . (' PLOOOP')<script>"; 
+    if(isset($_POST["Apa"]) && isset($_POST["Giraff"]) && isset($_POST["Coconut"]) && isset($_POST["Tiger"])) {
+
+        $Apa = $_POST["Apa"]; 
+        $Giraff = $_POST["Giraff"]; 
+        $Coconut = $_POST["Coconut"]; 
+        $Tiger = $_POST["Tiger"]; 
+
+
+
+        for ($i = 0; $i < $Apa; $i++) {
+            $rawData = file_get_contents("https://randomuser.me/api/");
+
+
+        } 
+        
+        for ($i = 0; $i < $Giraff; $i++) {
+            $rawData = file_get_contents("https://randomuser.me/api/");
+        
+
+        }
+
+        for ($i = 0; $i < $Coconut; $i++) {
+            $rawData = file_get_contents("https://randomuser.me/api/");
+        
+
+        }
+
+        for ($i = 0; $i < $Tiger; $i++) {
+            $rawData = file_get_contents("https://randomuser.me/api/");
+        
+
+        }
+
+
 
     }
+
+
 
 }
-
-
 
 
 
